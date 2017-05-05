893c30f27be5243c97adf20dec49c837df3a6158
Valente Giuliano <Valente Giuliano>
2017-05-05 10:52:01 +0200

    Alles

--- Fahrplan2/MainForm.Designer.cs
+++ Fahrplan2/MainForm.Designer.cs
@@ -43,6 +43,13 @@
             this.dTPTime = new System.Windows.Forms.DateTimePicker();
             this.Datum = new System.Windows.Forms.Label();
             this.LVtafel = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LabelFehler3 = new System.Windows.Forms.Label();
+            this.lkarte = new System.Windows.Forms.LinkLabel();
+            this.lkarte2 = new System.Windows.Forms.LinkLabel();
+            this.AutoCom = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // Stationtb
@@ -88,7 +95,7 @@
             // Von
             // 
             this.Von.AutoSize = true;
-            this.Von.Location = new System.Drawing.Point(23, 66);
+            this.Von.Location = new System.Drawing.Point(11, 66);
             this.Von.Name = "Von";
             this.Von.Size = new System.Drawing.Size(29, 13);
             this.Von.TabIndex = 48;
@@ -97,7 +104,7 @@
             // Nach
             // 
             this.Nach.AutoSize = true;
-            this.Nach.Location = new System.Drawing.Point(16, 98);
+            this.Nach.Location = new System.Drawing.Point(11, 98);
             this.Nach.Name = "Nach";
             this.Nach.Size = new System.Drawing.Size(36, 13);
             this.Nach.TabIndex = 49;
@@ -116,7 +123,7 @@
             // LabelFehler
             // 
             this.LabelFehler.AutoSize = true;
-            this.LabelFehler.Location = new System.Drawing.Point(76, 9);
+            this.LabelFehler.Location = new System.Drawing.Point(76, 41);
             this.LabelFehler.Name = "LabelFehler";
             this.LabelFehler.Size = new System.Drawing.Size(203, 13);
             this.LabelFehler.TabIndex = 56;
@@ -133,17 +140,16 @@
             // 
             // ComboListe
             // 
-            this.ComboListe.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
-            this.ComboListe.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.ComboListe.FormattingEnabled = true;
-            this.ComboListe.Location = new System.Drawing.Point(294, 66);
+            this.ComboListe.Location = new System.Drawing.Point(364, 65);
             this.ComboListe.Name = "ComboListe";
             this.ComboListe.Size = new System.Drawing.Size(200, 21);
             this.ComboListe.TabIndex = 61;
+            this.ComboListe.TextChanged += new System.EventHandler(this.ComboListe_TextChanged);
             // 
             // BtListe
             // 
-            this.BtListe.Location = new System.Drawing.Point(294, 98);
+            this.BtListe.Location = new System.Drawing.Point(364, 98);
             this.BtListe.Name = "BtListe";
             this.BtListe.Size = new System.Drawing.Size(75, 23);
             this.BtListe.TabIndex = 62;
@@ -175,11 +181,90 @@
             this.LVtafel.TabIndex = 65;
             this.LVtafel.UseCompatibleStateImageBehavior = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 24);
+            this.label1.TabIndex = 66;
+            this.label1.Text = "Fahrplan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(360, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 24);
+            this.label2.TabIndex = 67;
+            this.label2.Text = "Stationenliste";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(716, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 24);
+            this.label3.TabIndex = 68;
+            this.label3.Text = "Abfahrtstafel";
+            // 
+            // LabelFehler3
+            // 
+            this.LabelFehler3.AutoSize = true;
+            this.LabelFehler3.Location = new System.Drawing.Point(717, 41);
+            this.LabelFehler3.Name = "LabelFehler3";
+            this.LabelFehler3.Size = new System.Drawing.Size(203, 13);
+            this.LabelFehler3.TabIndex = 70;
+            this.LabelFehler3.Text = "Fehler! Sie müssen eine Station angeben.";
+            this.LabelFehler3.Visible = false;
+            // 
+            // lkarte
+            // 
+            this.lkarte.AutoSize = true;
+            this.lkarte.Location = new System.Drawing.Point(285, 69);
+            this.lkarte.Name = "lkarte";
+            this.lkarte.Size = new System.Drawing.Size(32, 13);
+            this.lkarte.TabIndex = 71;
+            this.lkarte.TabStop = true;
+            this.lkarte.Text = "Karte";
+            this.lkarte.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lkarte_LinkClicked);
+            // 
+            // lkarte2
+            // 
+            this.lkarte2.AutoSize = true;
+            this.lkarte2.Location = new System.Drawing.Point(285, 95);
+            this.lkarte2.Name = "lkarte2";
+            this.lkarte2.Size = new System.Drawing.Size(32, 13);
+            this.lkarte2.TabIndex = 72;
+            this.lkarte2.TabStop = true;
+            this.lkarte2.Text = "Karte";
+            this.lkarte2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lkarte2_LinkClicked);
+            // 
+            // AutoCom
+            // 
+            this.AutoCom.AutoSize = true;
+            this.AutoCom.Location = new System.Drawing.Point(484, 104);
+            this.AutoCom.Name = "AutoCom";
+            this.AutoCom.Size = new System.Drawing.Size(84, 17);
+            this.AutoCom.TabIndex = 73;
+            this.AutoCom.Text = "Automatisch";
+            this.AutoCom.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1221, 494);
+            this.Controls.Add(this.AutoCom);
+            this.Controls.Add(this.lkarte2);
+            this.Controls.Add(this.lkarte);
+            this.Controls.Add(this.LabelFehler3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.LVtafel);
             this.Controls.Add(this.Datum);
             this.Controls.Add(this.dTPTime);
@@ -218,6 +303,13 @@
         private System.Windows.Forms.DateTimePicker dTPTime;
         private System.Windows.Forms.Label Datum;
         private System.Windows.Forms.ListView LVtafel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LabelFehler3;
+        private System.Windows.Forms.LinkLabel lkarte;
+        private System.Windows.Forms.LinkLabel lkarte2;
+        private System.Windows.Forms.CheckBox AutoCom;
     }
 }
 
--- Fahrplan2/MainForm.cs
+++ Fahrplan2/MainForm.cs
@@ -14,6 +14,8 @@ namespace Fahrplan2
     public partial class MainForm : Form
     {
         private ITransport testee;
+        private bool needAutoCompleteUpdate = false;
+        AutoCompleteStringCollection autoComplete = new AutoCompleteStringCollection();
 
         public MainForm()
         {
@@ -25,90 +27,168 @@ namespace Fahrplan2
         }
 
         private void Search_Click(object sender, EventArgs e)
-        {           
-            if (Vontb.Text == "" || Nachtb.Text == "")
-                {
-                   LabelFehler.Visible = true;
-                }
-                    else
-                    {
-                        LVverbindung.Items.Clear();
-
-                        String inputTime = dTPTime.Text;
-                        var date = DateTime.Parse(inputTime.Substring(0, 10));
-                        String formatDate = date.ToString("yyyy-MM-dd");
-                        String time = inputTime.Substring(12, 6);
-
-                        testee = new Transport();
-                        var connections = testee.GetConnections(Vontb.Text, Nachtb.Text);
-              
-                        for (int i = 0; i < connections.ConnectionList.Count; i++)
-                        {
-                            Connection result = connections.ConnectionList[i];
-
-                            ConnectionPoint from = result.From;
-                            ConnectionPoint to = result.To;
-
-                            FahrplanVerbindungen verbindung = new FahrplanVerbindungen(from, to, result);
-
-                            var item = new ListViewItem(new[] { verbindung.getStartStation(), verbindung.getEndStation(), verbindung.getDeparture(), verbindung.getArrival(), verbindung.getDuration() });
-
-                            LVverbindung.Items.Add(item);
-                            
-                                                                          
-                        }
-                    Vontb.Clear();
-                    Nachtb.Clear();
-                }
-            }
-        private void Tabelle()
         {
+            if (Vontb.Text == "" || Nachtb.Text == "")
+            {
+                LabelFehler.Visible = true; // Fehlermeldung anzeigen lassen
+            }
+            else
+            {
+                LVverbindung.Items.Clear();
+
+                String inputTime = dTPTime.Text;
+                var date = DateTime.Parse(inputTime.Substring(0, 10));
+                String formatDate = date.ToString("yyyy-MM-dd");
+                String time = inputTime.Substring(12, 6); 
+
+                
+                var connections = testee.GetConnections(Vontb.Text, Nachtb.Text, formatDate, time);
+
+                for (int i = 0; i < connections.ConnectionList.Count; i++)
+                {
+                    Connection result = connections.ConnectionList[i];
+
+                    ConnectionPoint from = result.From;
+                    ConnectionPoint to = result.To;
+
+                    FahrplanVerbindungen verbindung = new FahrplanVerbindungen(from, to, result);
+
+                    var item = new ListViewItem(new[] { verbindung.getStartStation(), verbindung.getEndStation(), verbindung.getDeparture(), verbindung.getArrival(), verbindung.getDuration() });
+
+                    LVverbindung.Items.Add(item);
+
+
+                }
+                Vontb.Clear();
+                Nachtb.Clear();
+                LabelFehler.Visible = false;
+            }
+        }
+        private void Tabelle()
+        {   
+            // Tabelle Fahrplan
             LVverbindung.View = View.Details;
             LVverbindung.Columns.Add("Von:", 140, HorizontalAlignment.Left);
-            LVverbindung.Columns.Add("Abfahrt:", 120, HorizontalAlignment.Left);
-            LVverbindung.Columns.Add("Nach:", 140, HorizontalAlignment.Left);
+            LVverbindung.Columns.Add("Nach:", 120, HorizontalAlignment.Left);
+            LVverbindung.Columns.Add("Abfahrt:", 140, HorizontalAlignment.Left);
             LVverbindung.Columns.Add("Ankunft:", 120, HorizontalAlignment.Left);
             LVverbindung.Columns.Add("Dauer:", 100, HorizontalAlignment.Left);
 
+            // Tabelle Abfahrtstafel
             LVtafel.View = View.Details;
             LVtafel.Columns.Add("Abfahrt:", 120, HorizontalAlignment.Left);
-            LVtafel.Columns.Add("Katogerie:", 65, HorizontalAlignment.Left);
-            LVtafel.Columns.Add("Name:", 50, HorizontalAlignment.Left);
+            LVtafel.Columns.Add("Nummer:", 90, HorizontalAlignment.Left);
             LVtafel.Columns.Add("Von:", 130, HorizontalAlignment.Left);
             LVtafel.Columns.Add("Nach:", 130, HorizontalAlignment.Left);
         }
-      
+
         private void button2_Click(object sender, EventArgs e)
         {
-            String eingabe =  ComboListe.Text;
+                String eingabe = ComboListe.Text;
 
-            var stations =  testee.GetStations(eingabe);
+                var stations = testee.GetStations(eingabe);
 
-            foreach (Station stationName in stations.StationList)
-            {
-                ComboListe.Items.Add(stationName.Name);
-            }
+                foreach (Station stationName in stations.StationList)
+                {
+                    ComboListe.Items.Add(stationName.Name);
+                }          
         }
-
         private void Search2_Click(object sender, EventArgs e)
         {
             LVtafel.Items.Clear();
 
-            testee = new Transport();
-            Stations stations = testee.GetStations(Stationtb.Text);
-            Station station = stations.StationList[0];
-            String id = station.Id;
-
-            StationBoardRoot stationBoard = testee.GetStationBoard(Stationtb.Text, id);
-
-            foreach (StationBoard entries in stationBoard.Entries)
+            if (Stationtb.Text == "")
             {
-                var item = new ListViewItem(new[] { entries.Stop.Departure.ToString(), entries.Category, entries.Name, station.Name, entries.To });
-                LVtafel.Items.Add(item);
+                LabelFehler3.Visible = true; // Fehlermeldung anzeigen lassen
             }
-            Stationtb.Clear();
+            else
+            {
+                testee = new Transport();
+                Stations stations = testee.GetStations(Stationtb.Text);
+                Station station = stations.StationList[0];
+                String id = station.Id;
+
+                StationBoardRoot stationBoard = testee.GetStationBoard(Stationtb.Text, id);
+
+                foreach (StationBoard entries in stationBoard.Entries)
+                {
+                    var item = new ListViewItem(new[] { entries.Stop.Departure.ToString(), entries.Name, station.Name, entries.To });
+                    LVtafel.Items.Add(item);
+                }
+                Stationtb.Clear();
+                LabelFehler3.Visible = false; 
+            }
+
+
         }
 
-       
-    } 
-}
+        private void lkarte_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            Stations stations = testee.GetStations(Vontb.Text);
+
+            if (stations.StationList.Count > 0)
+            {
+                Station station = stations.StationList[0];
+                System.Diagnostics.Process.Start("https://www.google.ch/maps/search/" + station.Name + " Haltestelle"); // Zeigt die Haltestelle bei Google Maps an
+            }
+            else
+            {
+                LabelFehler.Visible = true; // Fehlermeldung anzeigen lassen
+            }
+        }
+
+        private void lkarte2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Stations stations = testee.GetStations(Nachtb.Text);
+
+            if (stations.StationList.Count > 0)
+            {
+                Station station = stations.StationList[0];
+                System.Diagnostics.Process.Start("https://www.google.ch/maps/search/" + station.Name + " Haltestelle"); // Zeigt die Haltestelle bei Google Maps an
+            }
+            else
+            {
+                LabelFehler.Visible = true; // Fehlermeldung anzeigen lassen
+            }
+        }
+
+        private void ComboListe_TextChanged(object sender, EventArgs e)
+        {
+            {
+                if (AutoCom.Checked)
+                {
+
+                    var input = ComboListe.Text;
+
+                    if (input.Length >= 3)
+                    {
+                        needAutoCompleteUpdate = true;
+                    }
+                    else
+                    {
+                        needAutoCompleteUpdate = false;
+                    }
+                    if (needAutoCompleteUpdate)
+                    {
+                        var stations = testee.GetStations(input);
+
+                        foreach (Station stationName in stations.StationList)
+                        {
+                            ComboListe.AutoCompleteCustomSource.Add(stationName.Name);
+                        }
+                         this.ComboListe.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                         this.ComboListe.AutoCompleteSource = AutoCompleteSource.CustomSource;
+                         this.ComboListe.AutoCompleteCustomSource = autoComplete;
+                    }
+                }
+                else
+                {
+                    this.ComboListe.AutoCompleteMode = AutoCompleteMode.None;
+                }
+            }
+
+        }
+    }
+ }
+
